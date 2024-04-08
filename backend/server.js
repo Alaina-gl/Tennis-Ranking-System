c2de90ab200220c7920ce3d837a1bf748b863e38
Alaina-gl <108637123+Alaina-gl@users.noreply.github.com>
2024-04-08 14:38:14 -0700

    initiated MERN stack backend

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,22 @@
 // implement backend retrieval here
+require('dotenv').config();
+
+const express = require('express');
+const playerRoutes = require('./routes/players');
+
+// Create express app
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+// Routes
+app.use('/api/players', playerRoutes);
+
+// Listen for requests
+app.listen (process.env.PORT, () => console.log('Server running on port', process.env.PORT));
--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
 const App = () => {
-  return <div>Hello, welcome to Tennis-Ranking-Systems! Find your ranking here.</div>;
+  return <div>Hello, welcome to Tennis-Ranking-Systems! Find your rankings here.</div>;
 };
 export default App;
